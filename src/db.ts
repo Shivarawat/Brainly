b9585b4d2d88f9ee26e26c08e50255b8a57febee
Shiv Charan Rawat <shivcharan.rawat@shivcha-ltmjn6k.internal.salesforce.com>
2025-12-31 02:08:19 +0530

    Removed node_modules folder from tracking

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,10 @@
 const URI = 'mongodb+srv://admin:shiv1234@cluster0.8ggvq4l.mongodb.net/';
+
+import { model, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+    username: {type: String, unique: true},
+    password: String
+});
+
+export const UserModel = model( "User", UserSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
+import { UserModel } from './db.js';
 
 const app = express();
 
-app.post('/api/v1/signup', (req, res)=>{});
+app.post('/api/v1/signup', async (req, res)=>{
+    // TODO: zod validation, password hashing
+    const username = req.body.name;
+    const password = req.body.password;
+    await UserModel.create({
+        username: username, 
+        password: password
+    });
+    res.json({message: "User signed up"});
+});
 app.post('/api/v1/signin', (req, res)=>{});
 app.post('/api/v1/content', (req, res)=>{});
 app.get('/api/v1/content', (req, res)=>{});
